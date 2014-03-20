b3ad9075d6434848cb1446c126177b27eb8eb74c
Cliff Ingham <inghamn@bloomington.in.gov>
2014-03-20 10:31:51 -0400

    Drupal basepath is now added to the javascript for the browser popup window.
    Closes #1

--- cmis_field/js/cmis_field.js
+++ cmis_field/js/cmis_field.js
@@ -7,24 +7,17 @@
       $(".edit-field-cmis-field", context).after('<input type="button" id="edit-cmis-field" class="cmis-field-insert-button form-submit" value="Browse" />');
       $("#edit-instance-settings-cmis-field-rootfolderpath", context).after('<input type="button" id="edit-cmis-settings" class="cmis-field-insert-button form-submit" value="Browse" />');
       $('.cmis-field-insert-button').click(function() {
-        var caller = '';
-        if ($(this).attr('id') == 'edit-cmis-settings') {
-          caller = 'settings';
-        }
-        else {
-          caller = 'widget';
-        }
-        var rootDir = Drupal.settings.cmispath;
-        //TODO: Make this a modal, not a popup (will be much harder to manage context when clicking around the cmis browser)
-        if (rootDir == true){
-	      rootDir === '';
-	      window.open('/cmis/browser?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
-        }else{
-          window.open('/cmis/browser' + rootDir + '?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
-        }
-        return false;
+          var browserUrl = Drupal.settings.basePath + 'cmis/browser',
+              rootDir    = Drupal.settings.cmispath,
+              caller     = ($(this).attr('id') == 'edit-cmis-settings') ? 'settings' : 'widget';
+
+          if (rootDir) { browserUrl += rootDir; }
+
+          //TODO: Make this a modal, not a popup (will be much harder to manage context when clicking around the cmis browser)
+          window.open(browserUrl + '?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
+          return false;
       });
-      
+
       $(".cmis-field-insert", context).click(function() {
         if($.query['caller'] == 'settings') {
           var cmispath = $(this).attr('href');
@@ -33,16 +26,16 @@
         else {
           var cmispath = $(this).attr('id');
           var cmisname = $(this).attr('name');
-          //$('#edit-field-cmis-und-0-title', window.opener.document).val(cmisname);          
+          //$('#edit-field-cmis-und-0-title', window.opener.document).val(cmisname);
           $('.edit-field-cmis-field', window.opener.document).val(cmisname);
-          $('.edit-field-cmis-path', window.opener.document).val(cmispath);
+          $('.edit-field-cmis-path',  window.opener.document).val(cmispath);
         }
         window.close();
         return false;
       });
     }
   };
-  
+
   $.query = (function(a) {
     if (a == "") return {};
     var b = {};
